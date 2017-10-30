99fc535e1efad15cc6c8924628eaf60ec4107df8
AChin1311 <daphne011552000@yahoo.com.tw>
2017-10-30 15:19:11 -0700

    update parse.py

--- parse.py
+++ parse.py
@@ -10,6 +10,7 @@ for line in tweets_file:
     try:
         tweet = json.loads(line)
         tweets_data.append(tweet)
+        print(tweet['text'])
     except:
         continue
 
@@ -20,13 +21,14 @@ tweets['text'] = map(lambda tweet: tweet['text'], tweets_data)
 tweets['lang'] = map(lambda tweet: tweet['lang'], tweets_data)
 tweets['country'] = map(lambda tweet: tweet['place']['country'] if tweet['place'] != None else None, tweets_data)
 
-tweets_by_lang = tweets['lang'].value_counts()
 
-fig, ax = plt.subplots()
-ax.tick_params(axis='x', labelsize=15)
-ax.tick_params(axis='y', labelsize=10)
-ax.set_xlabel('Languages', fontsize=15)
-ax.set_ylabel('Number of tweets' , fontsize=15)
-ax.set_title('Top 5 languages', fontsize=15, fontweight='bold')
-tweets_by_lang[:5].plot(ax=ax, kind='bar', color='red')
+# tweets_by_lang = tweets['lang'].value_counts()
+
+# fig, ax = plt.subplots()
+# ax.tick_params(axis='x', labelsize=15)
+# ax.tick_params(axis='y', labelsize=10)
+# ax.set_xlabel('Languages', fontsize=15)
+# ax.set_ylabel('Number of tweets' , fontsize=15)
+# ax.set_title('Top 5 languages', fontsize=15, fontweight='bold')
+# tweets_by_lang[:5].plot(ax=ax, kind='bar', color='red')
 
