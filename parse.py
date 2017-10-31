2cf8b896ff968bca643b6880108d6ff466c6059f
Calvin Chen <calvinyhchen@Cs-210-33.CS.UCLA.EDU>
2017-10-31 14:44:34 -0700

    solve python3 map problem and plot top 5 country

--- parse.py
+++ parse.py
@@ -2,6 +2,7 @@ import json
 import pandas as pd
 import matplotlib.pyplot as plt
 import sys
+
 tweets_data_path = sys.argv[1]
 
 tweets_data = []
@@ -10,29 +11,33 @@ for line in tweets_file:
     try:
         tweet = json.loads(line)
         tweets_data.append(tweet)
-        print(tweet['text'])
-        print(tweet['created_at'])
+        #print(tweet['text'])
     except:
         continue
 
 print(len(tweets_data))
 
 tweets = pd.DataFrame()
-tweets['text'] = map(lambda tweet: tweet['text'], tweets_data)
-tweets['lang'] = map(lambda tweet: tweet['lang'], tweets_data)
-tweets['country'] = map(lambda tweet: tweet['place']['country'] if tweet['place'] != None else None, tweets_data)
-tweets['created_at'] = map(lambda tweet: tweet['created_at'], tweets_data)
+tweets['text'] = list(map(lambda tweet: tweet['text'], tweets_data))
+tweets['lang'] = list(map(lambda tweet: tweet['lang'], tweets_data))
+tweets['country'] = list(map(lambda tweet: tweet['place']['country'] if tweet['place'] != None else None, tweets_data))
+tweets['created_at'] = list(map(lambda tweet: tweet['created_at'], tweets_data))
 
+for i in range(len(tweets_data)):
+    print(tweets['text'][i])
+    if(tweets['country'][i]!=None):
+        print(tweets['country'][i])
+    print(tweets['created_at'][i])
+    
 
-
-#
-# tweets_by_lang = tweets['lang'].value_counts()
-
-# fig, ax = plt.subplots()
-# ax.tick_params(axis='x', labelsize=15)
-# ax.tick_params(axis='y', labelsize=10)
-# ax.set_xlabel('Languages', fontsize=15)
-# ax.set_ylabel('Number of tweets' , fontsize=15)
-# ax.set_title('Top 5 languages', fontsize=15, fontweight='bold')
-# tweets_by_lang[:5].plot(ax=ax, kind='bar', color='red')
+#Plot top 5 country
+tweets_by_country = tweets['country'].value_counts()
+fig, ax = plt.subplots()
+ax.tick_params(axis='x', labelsize=15)
+ax.tick_params(axis='y', labelsize=10)
+ax.set_xlabel('Country', fontsize=15)
+ax.set_ylabel('Number of tweets' , fontsize=15)
+ax.set_title('Top 5 Country', fontsize=15, fontweight='bold')
+tweets_by_country[:5].plot(ax=ax, kind='bar', color='red')
+plt.show()
 
--- test.py
+++
@@ -1,5 +0,0 @@
-consumer_key = "TAZEcFcEAifr6PjoQDcgOgwB7"
-consumer_secret = "ZNDHQdVIz7z1GNQtExVqJb2YYxlZB1AglDdEPl2YPoII7rGT8n"
-access_token = "922341627678531584-G2w4ZlE5VXriPrU2yTeggpLNqBOvfrY"
-access_token_secret = "iWmx5UOBej2IV1OkLiIpPbCP9M4NwYYNIoyWzIk5Bspw0"
-
