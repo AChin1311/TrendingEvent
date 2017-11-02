fed5309e58932821b7ca2c6d1a93dd6b4184ce42
Calvin Chen <calvinyhchen@CalvindeMacBook-Pro.local>
2017-11-02 12:18:50 -0700

    format for submit

---
+++ code/streaming.py
@@ -0,0 +1,34 @@
+#Import the necessary methods from tweepy library
+from tweepy.streaming import StreamListener
+from tweepy import OAuthHandler
+from tweepy import Stream
+import sys
+import keys
+import json
+import kw
+
+#This is a basic listener that just prints received tweets to stdout.
+class StdOutListener(StreamListener):
+    
+    def on_data(self, data):
+        with open(sys.argv[1], 'a') as f:
+          tweet = json.loads(data)
+          f.write(data)
+          print('write')
+        return True
+
+    def on_error(self, status):
+        print(status)
+
+if __name__ == '__main__':
+
+    #This handles Twitter authetification and the connection to Twitter Streaming API
+    l = StdOutListener()
+    auth = OAuthHandler(keys.consumer_key, keys.consumer_secret)
+    auth.set_access_token(keys.access_token, keys.access_token_secret)
+    stream = Stream(auth, l)
+
+    #This line filter Twitter Streams to capture data by the keywords: 'python', 'javascript', 'ruby'
+    stream.filter(track = kw.date+kw.disaster, languages=['en'])
+
+
---
+++ run.sh
@@ -0,0 +1,2 @@
+now="$(date +'%m%d')"
+python3 ./code/streaming.py ./code/$now.json
