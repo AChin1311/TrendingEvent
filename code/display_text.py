7940f38df522ea08398aeb1d833d6eeb2ef04d30
AChin1311 <daphne011552000@yahoo.com.tw>
2017-11-27 13:28:57 -0800

    generate feature vector

--- code/display_text.py
+++ code/display_text.py
@@ -2,56 +2,43 @@ import json
 import pandas as pd
 import sys
 import time
+from kw import features as kw_features
 
 tweets_data_path = sys.argv[1]
 
 tweets_data = []
+tweets_text = []
 tweets_file = open(tweets_data_path, "r")
 for line in tweets_file:
     try:
-        tweet = json.loads(line).lower()
+        tweet = json.loads(line)
+        tweets_text.append(tweet['text'].lower())
         tweets_data.append(tweet)
+        
         print(tweet['text'])
-        print()
     except:
         continue
 
 print("Total Msg:")
 print(len(tweets_data))
 
-for tw in tweets_data:
-    disaster = 0
-    data = 0
-    month = 0
-    entertainment = 0
-    holiday = 0
-    action = 0
-    price = 0
-    time = 0
-    logistics = 0
-    sport = 0
-    location = 0
-    for w in tw:
-        if w in kw.disaster:
-            disaster += 1
-        if w in kw.data:
-            data += 1
-        if w in kw.month:
-            month += 1
-        if w in kw.entertainment:
-            entertainment += 1
-        if w in kw.holiday:
-            holiday += 1
-        if w in kw.action:
-            action += 1
-        if w in kw.price:
-            price += 1
-        if w in kw.time:
-            time += 1
-        if w in kw.logistics:
-            logistics += 1
-        if w in kw.sport:
-            sport += 1
-        if w in kw.location:
-            location += 1
 
+vectors = []
+for tw in tweets_text:
+    li = ['disaster','date','month','entertainment','holiday','action','price','time','logistics','sport','location']
+    features ={}
+    for l in li:
+        features[l] = 0
+
+    for w in tw.split():
+        for l in li:
+            if w in kw_features[l]:
+                features[l] += 1
+
+    tmp = []
+    for l in features:
+        tmp.append(features[l])
+    if sum(tmp) == 0:
+        continue
+    vectors.append(tmp)
+print(vectors)
