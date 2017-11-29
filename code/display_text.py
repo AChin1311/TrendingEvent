deb7cbbb901a484d60dcf4389d207b27909ab455
Calvin Chen <calvinyhchen@CalvindeMacBook-Pro.local>
2017-11-29 13:30:03 -0800

    compress json

--- code/display_text.py
+++ code/display_text.py
@@ -4,55 +4,65 @@ import sys
 import time
 from kw import features as kw_features
 
-tweets_data_path = sys.argv[1]
 
-tweets_data = []
-tweets_text = []
-tweets_file = open(tweets_data_path, "r")
-for line in tweets_file:
-    try:
-        tweet = json.loads(line)
-        tweets_text.append(tweet['text'].lower())
-        tweets_data.append(tweet)
-        
-        #print(tweet['text'])
-    except:
-        continue
+def compress(tw_d):
+    return json.dumps(tw_d) + '\n'    
 
-print("Total Msg:")
-print(len(tweets_data))
+if __name__ == '__main__':
+
+    tweets_data_path = sys.argv[1]
+
+    tweets_data = []
+    tweets_text = []
+    tweets_file = open(tweets_data_path, "r")
+    for line in tweets_file:
+        try:
+            tweet = json.loads(line)
+            tweets_text.append(tweet['text'].lower())
+            tweets_data.append(tweet)
+            
+            #print(tweet['text'])
+        except:
+            continue
+
+    print("Total Msg:")
+    print(len(tweets_data))
 
 
-vectors = []
-for tw in tweets_text:
-    li_type = ['disaster','entertainment','holiday','sport']
-    li_details = ['date','month','action','price','time','logistics','location']
-    features_type ={}
-    features_details ={}
-    for l in li_type:
-        features_type[l] = 0
-    for l in li_details:
-        features_details[l] = 0
+    vectors = []
+    with open(sys.argv[1].split('.')[0] + '_compress.json' , 'w') as f:
+        for tw_d in tweets_data:
+            tw = tw_d['text']
+            li_type = ['disaster','entertainment','holiday','sport']
+            li_details = ['date','month','action','price','time','logistics','location']
+            features_type ={}
+            features_details ={}
+            for l in li_type:
+                features_type[l] = 0
+            for l in li_details:
+                features_details[l] = 0
 
-    for w in tw.split():
-        for l in li_type:
-            if w in kw_features[l]:
-                features_type[l] += 1
-        for l in li_details:
-            if w in kw_features[l]:
-                features_details[l] += 1
+            for w in tw.split():
+                for l in li_type:
+                    if w in kw_features[l]:
+                        features_type[l] += 1
+                for l in li_details:
+                    if w in kw_features[l]:
+                        features_details[l] += 1
 
-    tmp_type = []
-    tmp_deatails = []
-    for l in features_type:
-        tmp_type.append(features_type[l])
-    for l in features_details:
-        tmp_deatails.append(features_details[l])
-    if sum(tmp_type) == 0 or sum(tmp_deatails) == 0:
-        continue
-    vectors.append(tmp_type + tmp_deatails)
-    print(tmp_type + tmp_deatails)
-    print(tw)
+            tmp_type = []
+            tmp_deatails = []
+            for l in features_type:
+                tmp_type.append(features_type[l])
+            for l in features_details:
+                tmp_deatails.append(features_details[l])
+            if sum(tmp_type) == 0 or sum(tmp_deatails) == 0:
+                continue
+            vectors.append(tmp_type + tmp_deatails)
+            print(tmp_type + tmp_deatails)
+            f.write(compress(tw_d))
+
+    
 
 
 #print(vectors)
