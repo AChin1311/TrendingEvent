b4c2f5544e69d42859c6f2a462759a0fb100bc78
AChin1311 <daphne011552000@yahoo.com.tw>
2017-12-01 20:23:26 -0800

    predict results with kmeans

--- code/kmeans.py
+++ code/kmeans.py
@@ -1,5 +1,12 @@
+import gensim
 from sklearn.cluster import KMeans
+model = gensim.models.doc2vec.Doc2Vec.load('doc2vec.model')
+#start testing
+#printing the vector of document at index 1 in docLabels
+X = model.docvecs
 
 kmeans = KMeans(n_clusters=4)
 kmeans.fit(X)
 y_kmeans = kmeans.predict(X)
+for y in y_kmeans:
+  print(y)
